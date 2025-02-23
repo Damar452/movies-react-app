89185bbe2115d0f73976520e9efe7d6fe51bd4bb
Damar Hernandez <damarh452@gmail.com>
2025-02-23 04:12:49 -0500

    Remove unused import

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import AppRoutes from './routes/index';
 import App from './App';
 import '@ant-design/v5-patch-for-react-19';
 import 'antd/dist/reset.css';
